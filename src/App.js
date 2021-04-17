b8c8f978cea6d0ad21d2dc5bbe5adc1b5a1e8f00
Ikeh Leonard <leoikeh99@gmail.com>
2021-04-17 17:06:07 +0100

    made responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Question from "./components/Question";
 import Results from "./components/Results";
 import { getQuestions } from "./functions";
 import background from "./images/background.png";
 import "./App.css";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [questions, setQuestions] = useState(null);
@@ -20,18 +21,27 @@ function App() {
   return (
     <div className="app">
       <img src={background} alt="" className="background" />
-      <h1>COUNTRY QUIZ</h1>
-      {questions && stage < 10 && (
-        <Question
-          question={questions[stage]}
-          setStage={setStage}
-          stage={stage}
-          result={result}
-          setResult={setResult}
-        />
-      )}
 
-      {stage > 9 && <Results result={result} />}
+      {questions ? (
+        <Fragment>
+          <h1>COUNTRY QUIZ</h1>
+          {questions && stage < 10 && (
+            <Question
+              question={questions[stage]}
+              setStage={setStage}
+              stage={stage}
+              result={result}
+              setResult={setResult}
+            />
+          )}
+
+          {stage > 9 && <Results result={result} />}
+        </Fragment>
+      ) : (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Spinner />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="spinner">
+      <div class="spinner-item"></div>
+      <div class="spinner-item"></div>
+      <div class="spinner-item"></div>
+    </div>
+  );
+};
+
+export default Spinner;
